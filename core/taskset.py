265c2fa5a10103a220a9ec14805edd63295d2748
Saber <pouyamohammadyirbu@gmail.com>
2023-07-09 00:05:09 -0700

    adding task loader

--- core/taskset.py
+++ core/taskset.py
@@ -1,4 +1,6 @@
 # Handles my task set information
+import json
+
 class TaskSet:
     def __init__(self) -> None:
         self.tasks = list()
@@ -6,6 +8,14 @@ class TaskSet:
 
     def update_jobs(self, cpu_time):
         pass
-    
-    def load_tasks_from_json(self):
-        pass
+
+    def load_tasks_from_json(self, name: str) -> int:
+        try:
+            with open("tasks/" + name + ".json", "r") as json_raw_data:
+                json_data = json.load(json_raw_data)
+                self.tasks = json_data['tasks']
+                print(self.tasks)
+                return 0 # success
+        except:
+            self.tasks = list()
+            return -1
