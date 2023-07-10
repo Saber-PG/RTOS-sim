b38e17cf9b060645775d06501b1ae952be420daf
Saber <pouyamohammadyirbu@gmail.com>
2023-07-09 23:07:25 -0700

    adding a simple application runnner

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Here I will build an RTOS sim - this is main of my program")
+from core.RTOS import RTOS
+from core.taskset import TaskSet
+from core.schedulers.EDF import EDF
+# Launch application in this file
+
+# Defaults:
+DEBUG = False
+SCHEDULER = 'EDF'
+TASKSET = 'taskset1'
+
+# build and attack main program modules
+edf = EDF()
+ts = TaskSet()
+ts.load_tasks_from_json(TASKSET)
+rtos = RTOS(ts, edf)
